6c8f8239dc6584eff92d233f6618de996dbdde54
CUADJ011 <juan.x.cuadros.-nd@disney.com>
2021-07-30 13:48:45 -0500

    adding another test to the suite.

---
+++ src/test/ThirdClass.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class ThirdClass {
+
+    @Test
+    public void webLoginHomeCarLoan() {
+        System.out.println("webLoginHome");
+    }
+
+    @Test
+    public void mobileLoginHomeCarLoan() {
+        System.out.println("mobileLoginHome");
+    }
+
+    @Test
+    public void apiLoginHomeCarLoan() {
+        System.out.println("apiLoginHome");
+    }
+}
