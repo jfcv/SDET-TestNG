b9ea8d39070f846f8a424133066ae273c715f501
CUADJ011 <juan.x.cuadros.-nd@disney.com>
2021-07-30 11:42:21 -0500

    basic scaffolding. Two classes created. [.xml] file added to the tests to execute all tests from every class from one single file.

---
+++ src/test/BasicTests.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class BasicTests {
+    @Test
+    public void demo() {
+        System.out.println("hellou");
+    }
+
+    @Test
+    public void secondTest() {
+        System.out.println("bye");
+    }
+}
---
+++ src/test/SecondClass.java
@@ -0,0 +1,10 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class SecondClass {
+    @Test
+    public void thirdTest() {
+        System.out.println("third test in a different class");
+    }
+}
