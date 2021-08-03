4d456316806852f470ed4e178cee4c57efc15932
CUADJ011 <juan.x.cuadros.-nd@disney.com>
2021-08-03 17:32:31 -0500

    Listeners on testng.

---
+++ src/test/Listeners.java
@@ -0,0 +1,50 @@
+package test;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class Listeners implements ITestListener {
+    @Override
+    public void onTestStart(ITestResult result) {
+
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        System.out.println("I successfully executed Listeners Pass Code");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        /*
+         * screenshot code
+         * response if the [API, mobile] failed
+         */
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+
+    }
+
+    @Override
+    public void onTestFailedWithTimeout(ITestResult result) {
+
+    }
+
+    @Override
+    public void onStart(ITestContext context) {
+
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+
+    }
+}
